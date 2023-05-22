4437b5c3a0209286cbad2424a38e0a5c4afc4f0a
dmonakh <d.monakh@it-sprout.org>
2023-05-22 23:06:11 +0300

    test cd

--- terraform_gcp/scripts/gcloud-auth-plugin.sh
+++ terraform_gcp/scripts/gcloud-auth-plugin.sh
@@ -1,10 +1,8 @@
  #!/bin/bash
 
 REPO_URL="https://packages.cloud.google.com/apt"
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-# echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] $REPO_URL cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] $REPO_URL cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list >/dev/null
 sudo wget -q https://packages.cloud.google.com/apt/doc/apt-key.gpg -O /usr/share/keyrings/cloud.google.gpg
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 
 sudo apt-get update -y
 sudo apt-get install -y google-cloud-sdk-gke-gcloud-auth-plugin
