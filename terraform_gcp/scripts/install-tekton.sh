4a1017a4894109a301de470100890da41eee8ab4
dmonakh <d.monakh@it-sprout.org>
2023-05-31 21:20:47 +0300

    destroy

--- terraform_gcp/scripts/install-tekton.sh
+++ terraform_gcp/scripts/install-tekton.sh
@@ -19,35 +19,35 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers
 # Install Tekton Operators
 kubectl apply -f https://storage.googleapis.com/tekton-releases/operator/latest/release.yaml
 
-if ! curl -s -H "Authorization: token $TOKEN_TEKTON" https://api.github.com/user/keys | grep -q "Tekton SSH Key"; then
-    ssh-keygen -t rsa -b 4096 -C "tekton@tekton.dev" -f tekton_key
-    # save as tekton / tekton_key.pub
-    # Get the public key contents
-    public_key=$(cat tekton_key.pub)
-    # Create a new SSH key on GitHub
-    curl -X POST -H "Authorization: token $TOKEN_TEKTON" -d '{"title":"Tekton SSH Key","key":"'"${public_key}"'"}' https://api.github.com/user/keys
+# if ! curl -s -H "Authorization: token $TOKEN_TEKTON" https://api.github.com/user/keys | grep -q "Tekton SSH Key"; then
+#     ssh-keygen -t rsa -b 4096 -C "tekton@tekton.dev" -f tekton_key
+#     # save as tekton / tekton_key.pub
+#     # Get the public key contents
+#     public_key=$(cat tekton_key.pub)
+#     # Create a new SSH key on GitHub
+#     curl -X POST -H "Authorization: token $TOKEN_TEKTON" -d '{"title":"Tekton SSH Key","key":"'"${public_key}"'"}' https://api.github.com/user/keys
 
-# create secret YAML from contents
-cat tekton_key | base64 -w 0 > tekton_key_base64.txt
-cat > tekton-git-ssh-secret.yaml << EOM
-apiVersion: v1
-kind: Secret
-metadata:
-    name: git-ssh-key
-    annotations:
-        tekton.dev/git-0: github.com
-type: kubernetes.io/ssh-auth
-data:
-    ssh-privatekey: $(cat tekton_key_base64.txt)
----
-EOM
+# # create secret YAML from contents
+# cat tekton_key | base64 -w 0 > tekton_key_base64.txt
+# cat > tekton-git-ssh-secret.yaml << EOM
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#     name: git-ssh-key
+#     annotations:
+#         tekton.dev/git-0: github.com
+# type: kubernetes.io/ssh-auth
+# data:
+#     ssh-privatekey: $(cat tekton_key_base64.txt)
+# ---
+# EOM
 
-kubectl apply -f tekton-git-ssh-secret.yaml
-else    
-    # Tekton SSH key exists on GitHub, skip generating and adding it
-    echo "Tekton SSH key already exists on GitHub"
+# kubectl apply -f tekton-git-ssh-secret.yaml
+# else    
+#     # Tekton SSH key exists on GitHub, skip generating and adding it
+#     echo "Tekton SSH key already exists on GitHub"
 
-fi   
+# fi   
 # create your Docker registry secret
 cat > secret-sa.yml << EOM
 apiVersion: v1
