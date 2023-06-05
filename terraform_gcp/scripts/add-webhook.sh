0eba19142f8d4b91d0fe23bb6f99af34b3606593
dmonakh <d.monakh@it-sprout.org>
2023-06-05 13:01:56 +0300

    add webhook

---
+++ terraform_gcp/scripts/add-webhook.sh
@@ -0,0 +1,20 @@
+gh api repos/dmonakh/Wordpress-Tekton-atK8s-inGCP/hooks \
+   --input - <<< '{
+  "name": "web",
+  "active": true,
+  "events": [
+    "watch"
+  ],
+  "config": {
+    "url": "http://34.70.146.116/hooks",
+    "content_type": "json"
+  }
+}'
+
+# gh repo create-commit --allow-empty -m "Empty commit"
+
+# gh repo push
+
+git commit --allow-empty -m "Empty-Commit"
+
+git push origin main
