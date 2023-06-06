943b282fcb4c85ec3549621aa13ad41b7cfba0e1
dmonakh <d.monakh@it-sprout.org>
2023-06-06 13:52:25 +0300

    test

--- terraform_gcp/scripts/setup-db-wp.sh
+++ terraform_gcp/scripts/setup-db-wp.sh
@@ -7,7 +7,36 @@ mysql -h $SERVER_NAME -u $USER_NAME -p$PASSWORD -P 3306 -e "\
     USE WordPress; \
     CREATE TABLE IF NOT EXISTS TableforWP (id INT PRIMARY KEY, name VARCHAR(50)); \
     INSERT IGNORE INTO TableforWP (id, name) VALUES (1, 'Den'), (2, 'Max'), (3, 'Len');"                 
-if [  -f /var/www/html/wp-config.php ]; then  
+# if [  -f /var/www/html/wp-config.php ]; then  
+#     wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
+#     wp --allow-root --path=/var/www/html theme install hestia    && \
+#     wp --allow-root --path=/var/www/html theme activate hestia     
+# else    
+#     # Configure WP-CLI and install plugins and themes
+#     wp core download --path=/var/www/html --allow-root && \
+#     wp --allow-root --path=/var/www/html config create \
+#       --dbhost=mysql \
+#       --dbname=WordPress \
+#       --dbuser=root \
+#       --dbpass=wordsec \
+#       --allow-root && \
+#     wp --allow-root --path=/var/www/html core install \
+#       --url=localhost  \
+#       --title=WPsite \
+#       --admin_user=dmonakh \
+#       --admin_password=monakh3108d \
+#       --admin_email=den3108@mon.com && \
+#     wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
+#     wp --allow-root --path=/var/www/html theme install twentytwentytwo  && \
+#     wp --allow-root --path=/var/www/html theme activate twentytwentytwo     
+# fi
+if [ -f /var/www/html/wp-config.php ]; then  
+    wp --allow-root --path=/var/www/html core install \
+      --url=localhost  \
+      --title=WPsite \
+      --admin_user=dmonakh \
+      --admin_password=monakh3108d \
+      --admin_email=den3108@mon.com && \
     wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
     wp --allow-root --path=/var/www/html theme install hestia    && \
     wp --allow-root --path=/var/www/html theme activate hestia     
