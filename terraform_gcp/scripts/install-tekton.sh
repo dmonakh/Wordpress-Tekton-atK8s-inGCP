943ce038168bb919c2071315a1ac92a87aa52f9c
dmonakh <d.monakh@it-sprout.org>
2023-06-12 20:42:22 +0300

    init

--- terraform_gcp/scripts/install-tekton.sh
+++ terraform_gcp/scripts/install-tekton.sh
@@ -17,37 +17,37 @@ sleep 30
 echo "4 ----- >Install tasks from Tekton Hub"
 tkn hub install task git-clone && tkn hub install task buildah && tkn hub install task kubernetes-actions 
 
-# Install Tekton Triggers
-sleep 30
-echo "5 ----- >Install Tekton Triggers"
-kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+# # Install Tekton Triggers
+# sleep 30
+# echo "5 ----- >Install Tekton Triggers"
+# kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 
-# Install Tekton Operators
-echo "6 ----- >Install Tekton Operators"
-kubectl apply -f https://storage.googleapis.com/tekton-releases/operator/latest/release.yaml
+# # Install Tekton Operators
+# echo "6 ----- >Install Tekton Operators"
+# kubectl apply -f https://storage.googleapis.com/tekton-releases/operator/latest/release.yaml
 
-cat > regsecret.yaml << EOM
-kind: Secret
-apiVersion: v1
-metadata:
-  name: regsecret
-  annotations:
-    tekton.dev/docker-0: https://index.docker.io/
-type: kubernetes.io/basic-auth
-stringData:
-    username: $DOCKER_USERNAME
-    password: $DOCKER_PASSWORD
-EOM
+# cat > regsecret.yaml << EOM
+# kind: Secret
+# apiVersion: v1
+# metadata:
+#   name: regsecret
+#   annotations:
+#     tekton.dev/docker-0: https://index.docker.io/
+# type: kubernetes.io/basic-auth
+# stringData:
+#     username: $DOCKER_USERNAME
+#     password: $DOCKER_PASSWORD
+# EOM
 
-kubectl apply -f regsecret.yaml
+# kubectl apply -f regsecret.yaml
 
-echo "apply -f pipeline/"
-kubectl apply -f pipeline/
+# echo "apply -f pipeline/"
+# kubectl apply -f pipeline/
 
-echo "apply -f tekton/"
-kubectl apply -f tekton/
+# echo "apply -f tekton/"
+# kubectl apply -f tekton/
 
-kubectl create rolebinding pipeline-pvc --clusterrole=edit --serviceaccount=default:pipeline --namespace=default
-kubectl create clusterrolebinding pipeline-admin --clusterrole=cluster-admin --serviceaccount=default:pipeline
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.0/deploy/static/provider/cloud/deploy.yaml
+# kubectl create rolebinding pipeline-pvc --clusterrole=edit --serviceaccount=default:pipeline --namespace=default
+# kubectl create clusterrolebinding pipeline-admin --clusterrole=cluster-admin --serviceaccount=default:pipeline
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.0/deploy/static/provider/cloud/deploy.yaml
 
