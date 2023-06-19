c57353a0064cb98c5980d5defbf972422cedd665
dmonakh <d.monakh@it-sprout.org>
2023-06-19 15:02:06 +0300

    destroy

--- terraform_gcp/scripts/install-tekton.sh
+++ terraform_gcp/scripts/install-tekton.sh
@@ -50,4 +50,3 @@ kubectl apply -f tekton/
 kubectl create rolebinding pipeline-pvc --clusterrole=edit --serviceaccount=default:pipeline --namespace=default
 kubectl create clusterrolebinding pipeline-admin --clusterrole=cluster-admin --serviceaccount=default:pipeline
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.0/deploy/static/provider/cloud/deploy.yaml
-
