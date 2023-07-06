cb908adb01d0242eabfdd8362cf0333c0c9ca018
dmonakh <d.monakh@it-sprout.org>
2023-07-06 20:56:47 +0300

    init

---
+++ terraform_gcp/scripts/monitoring.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Install Helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+#Install Prometheus
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+kubectl create namespace prometheus
+helm install prometheus prometheus-community/prometheus --namespace prometheus
+
+#Install Grafana
+helm repo add grafana https://grafana.github.io/helm-charts
+kubectl create namespace grafana
+helm install grafana grafana/grafana --namespace grafana
